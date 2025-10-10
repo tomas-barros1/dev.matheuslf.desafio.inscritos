ed203444b5283c2425ea162a913673faaa420961
Matheus Leandro Ferreira <matheusleandroferreira@gmail.com>
2025-10-10 19:18:36 -0300

    First

---
+++ src/main/java/dev/matheuslf/desafio/inscritos/InscritosApplication.java
@@ -0,0 +1,13 @@
+package dev.matheuslf.desafio.inscritos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InscritosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InscritosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/matheuslf/desafio/inscritos/InscritosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.matheuslf.desafio.inscritos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InscritosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
